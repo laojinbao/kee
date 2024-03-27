86bbb1317911d22ead9fad15b9d1227f3b110ae8
刘林 <597852546@qq.com>
2024-03-27 18:47:31 +0800

    add pretter

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
@@ -107,6 +108,9 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
+      </div>
+      <div>
+        <Button>Click me</Button>
       </div>
     </main>
   )
